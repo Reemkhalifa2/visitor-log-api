6453f5d11f160acc9ff2d2a0de3ebce2c73c2370
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-07-14 15:53:28 +0400

    update serives

--- src/main/java/com/example/visitorlog/VisitorService.java
+++ src/main/java/com/example/visitorlog/VisitorService.java
@@ -10,18 +10,53 @@ import java.util.List;
 @Service
 public class VisitorService {
     private List<Visitor> visitors = new ArrayList<>();
-
+    private Integer count = 0;
     public List<Visitor> getAllVisitors(){
         return visitors;
     }
 
-    public Visitor getAllVisitors(Long id){
+    public Visitor getVisitorById(Long id){
         for(Visitor visitor : visitors){
             if(visitor.getId() == id){
                 return visitor;
             }
         }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Visitor not found");
+    }
+
+    public Visitor addVisitor(Visitor visitor) {
+        boolean exists = visitors.stream()
+                .anyMatch(v -> v.getId().equals(visitor.getId()));
+        if (exists) {
+            throw new RuntimeException("Visitor ID already exists");
+        }
+
+        visitors.add(visitor);
+        count ++;
+
+        return visitor;
+    }
+
+    public Visitor deleteVisitor(Visitor visitor) {
+        boolean exists = visitors.stream()
+                .anyMatch(v -> v.getId().equals(visitor.getId()));
+        if (exists) {
+            throw new RuntimeException("Visitor ID already exists");
+        }
+
+        visitors.remove(visitor);
+        count--;
+        return visitor;
+    }
+
+    public String totalVisitors(){
+        return "total: " + count;
+    }
+
+    public Visitor updateVisitor(long id, String purpose) {
+        Visitor visitor = getVisitorById(id);
+        visitor.setPurpose(purpose);
+        return visitor;
     }
 
 }
